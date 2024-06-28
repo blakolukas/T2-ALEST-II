8e248bb4324fb30e1adf50e636d3cc951c151298
blakolukas <lucasblacovicz@gmail.com>
2024-06-27 22:02:53 -0300

    com

--- src/Digraph.java
+++ src/Digraph.java
@@ -22,13 +22,23 @@ public class Digraph
         carrega(file);
     }
 
+    /*
+    carrega o arquivo
+    converte os números para int
+    adiciona em uma lista
+    ordena-a
+    inicializa um vérice com x,y,z da lista (não são medidas, são apenas váriaveis que ajudam a organizar) em ordem crescente para facilitar a comparação
+    irá comparar cada um com cada da lista
+    print para visualizar
+    toDot (Graphviz)
+    */
     private void carrega(String file){
         List<Vertex> vertices= new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
-                int[] nums= new int[3];
+                int[] nums= new int[3]; //organizar x,y,z
                 if (parts.length == 3) {
                     int x = Integer.parseInt(parts[0]);
                     nums[0]= x;
@@ -37,7 +47,7 @@ public class Digraph
                     int z = Integer.parseInt(parts[2]);
                     nums[2]= z;
                     Arrays.sort(nums);
-                    Vertex vertex = new Vertex(nums[2], nums[1], nums[0]);
+                    Vertex vertex = new Vertex(nums[2], nums[1], nums[0]); //adiciona em ordem crescente para ajudar a comparar
                     System.out.println(vertex);
                     vertices.add(vertex);
                 }
@@ -46,16 +56,18 @@ public class Digraph
             for (Vertex vertex : vertices) {
                 for(int i=0; i<vertices.size();i++){
                     if(vertex.compareTo(vertices.get(i))==-1){
-                        addEdge(vertex, vertices.get(i));
+                        addEdge(vertex, vertices.get(i)); //adiciona aresta caso seja menor
                     }
                 }
             }
             
+            //print
             for (Vertex vertex : vertices) {
                 System.out.println("\nvertice: "+vertex+"\n\n"+getAdj(vertex)+"\n\n-----------------------------------------"); 
             }
+
+            //toDot
             System.out.println("digraph {");
-            System.out.println("rankdir = LR;");
             System.out.println("node [shape = circle];");
             for (Vertex vertex : vertices) {
                 System.out.println(toDot(vertex));
@@ -67,10 +79,12 @@ public class Digraph
         }
     }
 
+    //adiciona aresta 
     public void addEdge(Vertex v, Vertex w) {
         addToList(v, w);
     }
 
+    //adiciona á lista de adjacentes do vértice (bag)
     private void addToList(Vertex v, Vertex w) {
         List<Vertex> list = graph.get(v);
         if(list == null){
@@ -79,7 +93,8 @@ public class Digraph
         list.add(w);
         graph.put(v, list);
     }
-
+    
+    //get adjacentes
     public Iterable<Vertex> getAdj(Vertex v){ 
         return graph.get(v); 
     }
@@ -89,6 +104,7 @@ public class Digraph
         return "Digraph [graph=" + graph + "]";
     }
 
+    //método toDot
     public String toDot(Vertex v) {
 		StringBuilder s = new StringBuilder();
         if(graph.get(v)==null){
